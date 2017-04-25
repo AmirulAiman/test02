7a1c593372af3fec7c7e1ef92810a2e66a2f180d
AokinoFuyu <aiman_amirul@ymail.com>
2017-04-26 05:04:55 +0800

    Edit admin.profile view

--- resources/views/layouts/admin/profile.blade.php
+++ resources/views/layouts/admin/profile.blade.php
@@ -1,13 +1,29 @@
 @extends('master')
 
-@inject('date', 'Carbon\Carbon')
-
 @section('title','Profile')
 
 @section('admin.profile','active')
 
 @section('content')
 <div>
-    Admin profile page
+    <div class="">
+        <hr>
+            <h4>Admin profile page</h4>
+        <hr>
+    </div>
+    <div class="panel panel-primary">
+    <div class="panel-heading">
+        <h5 class="panel-title">Detail</h5>
+    </div>
+    <div class="panel-body">
+        <h5><b>Email :</b> {{$profile->email }}</h5>
+        <h5><b>Last Login :</b> {{ $profile->last_login }}</h5>
+    </div>
+    <div class="panel-footer">
+        <div class="col-md-offset-11">
+            <a class="btn btn-warning" href="#">Edit</a>
+        </div>
+    </div>
+    </div>
 </div>
 @endsection
